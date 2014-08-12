752d727be9884d705c4a7a36b0696003bf9dae06
Salma <SalmaFG@github.com>
2014-08-12 23:44:13 +0300

    Collecting results and rearranging code

--- model.py
+++ model.py
@@ -4,80 +4,11 @@ import tempfile
 from time import sleep
 from fpconst import *
 import string
-# from Numeric import *
-# from MLab import *
 import numpy as np
 from numpy import array
 from numpy import *
 from sets import *
 import random
-
-data = [[1.0,0.56,0.13,1.0],
-        [2.0,0.59,0.13,1.0], 
-        [3.0,0.63,0.13,-1.0],
-        [4.0,0.66,0.13,-1.0],
-        [5.0,0.69,0.13,-1.0],
-        [6.0,0.72,0.13,-1.0],
-        [7.0,0.76,0.13,-1.0],
-        [8.0,0.79,0.13,-1.0],
-        [9.0,0.56,0.135,1.0],
-        [10.0,0.59,0.135,1.0],
-        [11.0,0.63,0.135,-1.0],
-        [12.0,0.66,0.135,-1.0],
-        [13.0,0.69,0.135,-1.0],
-        [14.0,0.72,0.135,-1.0],
-        [15.0,0.76,0.135,-1.0],
-        [16.0,0.79,0.135,-1.0],
-        [17.0,0.56,0.14,-1.0],
-        [18.0,0.59,0.14,-1.0],
-        [19.0,0.63,0.14,-1.0],
-        [20.0,0.66,0.14,-1.0],
-        [21.0,0.69,0.14,-1.0],
-        [22.0,0.72,0.14,-1.0],
-        [23.0,0.76,0.14,-1.0],
-        [24.0,0.79,0.14,-1.0],
-        [25.0,0.56,0.145,-1.0],
-        [26.0,0.59,0.145,-1.0],
-        [27.0,0.63,0.145,-1.0],
-        [28.0,0.66,0.145,-1.0],
-        [29.0,0.69,0.145,-1.0],
-        [30.0,0.72,0.145,-1.0],
-        [31.0, 0.76,0.145,-1.0],
-        [32.0,0.79,0.145,-1.0],
-        [33.0,0.56,0.15,-1.0],
-        [34.0,0.59,0.15,-1.0],
-        [35.0,0.63,0.15,-1.0],
-        [36.0,0.66,0.15,-1.0],
-        [37.0,0.69,0.15,-1.0],
-        [38.0,0.72,0.15,-1.0],
-        [39.0,0.76,0.15,-1.0],
-        [40.0,0.79,0.15,-1.0],
-        [41.0,0.56,0.155,-1.0],
-        [42.0,0.59,0.155,-1.0],
-        [43.0,0.63,0.155,-1.0],
-        [44.0,0.66,0.155,-1.0],
-        [45.0,0.69,0.155,-1.0],
-        [46.0,0.72,0.155,-1.0],
-        [47.0,0.76,0.155,-1.0],
-        [48.0,0.79,0.155,-1.0],
-        [49.0,0.56,0.16,-1.0],
-        [50.0,0.59,0.16,-1.0],
-        [51.0,0.63,0.16,-1.0],
-        [52.0,0.66,0.16,-1.0],
-        [53.0,0.69,0.16,-1.0],
-        [54.0,0.72,0.16,-1.0],
-        [55.0,0.76,0.16,2.0],
-        [56.0,0.79,0.16,2.0],
-        [57.0,0.56,0.165,-1.0],
-        [58.0,0.59,0.165,-1.0],
-        [59.0,0.63,0.165,-1.0],
-        [60.0,0.66,0.165,-1.0],
-        [61.0,0.69,0.165,-1.0],
-        [62.0,0.72,0.165,-1.0],
-        [63.0,0.76,0.165,2.0],
-        [64.0,0.79,0.165,2.0]]
-
-env = ['m','k','k','?']
 
 ##########################################################
 # SUSTAIN Class
@@ -196,7 +127,7 @@ class SUSTAIN:
             else:
                 accuracy = 1
                 self.adjustcluster(winnerindex, item, env)  
-        return [self.LAMBDAS, self.connections, self.clusters, int(floor(maskclus[3][1])), accuracy, len(self.clusters)]
+        return [self.LAMBDAS, self.connections, self.clusters, maskclus[3][1], accuracy, len(self.clusters)]
 
     ##########################################################
     #SALMA: Unsupervised learning for generalization mode:
@@ -225,10 +156,10 @@ class SUSTAIN:
 
             self.adjustcluster(winnerindex, cluster, env)
 
-            return [self.LAMBDAS, self.connections, self.clusters, cluster[3][1], 0, len(self.clusters)]
+            return [self.LAMBDAS, self.connections, self.clusters, cluster[3][1], len(self.clusters)]
         else:
-            # self.adjustclusterunsupervised(winnerindex, item, env)
-            return [self.LAMBDAS, self.connections, self.clusters, maskclus[3][1], 1, len(self.clusters)]       
+            self.adjustclusterunsupervised(winnerindex, item, env)
+            return [self.LAMBDAS, self.connections, self.clusters, int(round(maskclus[3][1])), len(self.clusters)]       
 
     ###########################################################
     # humbleteach: adjusts winning cluster (Equation #9 in Psych Review)
@@ -268,7 +199,9 @@ class SUSTAIN:
 
         self.LAMBDAS += map(lambda x,y: self.LEARN*x*(1.0-y), b, a)
 
-#####################################################
+    #############################################################
+    #SALMA: adjusts winning cluster in unsupervised learning mode
+    #############################################################
     def adjustclusterunsupervised(self, winner, item, env):
     
         catactsflat = resize(self.catunitacts,(1,len(self.catunitacts)*len(self.catunitacts[0])))[0]
@@ -291,78 +224,3 @@ class SUSTAIN:
 # END SUSTAIN Class
 ###########################################################
 
-subjectdata=[]
-directory=os.getcwd() + '/results/results.csv'
-dataitems = []
-for i in data:
-    row = []
-    for j in i:
-        row.append(np.array([0, j]))
-    dataitems.append(row)
-
-##########################################################
-  #SALMA: Training phase
-##########################################################
-def training(model,data):
-    phase='training'
-    examplenumbers=[1.0,2.0,9.0,10.0,55.0,56.0,63.0,64.0]
-    nblocks=4
-    nitemscorrect = 0
-    trainingblock = [dataitems[0],dataitems[1],dataitems[8],dataitems[9],
-    dataitems[54],dataitems[55],dataitems[62],dataitems[63]]
-
-    for i in range(nblocks):
-        random.shuffle(trainingblock)
-        for j in trainingblock:
-            # print j 
-            trialn=int(floor(j[0][1]))
-            [res,prob,outunits,outacts,act,dist] = model.stimulate(j, env)
-            [lambdas, clus, conn, response, accuracy, n] = model.learn(j,env)
-            trialdata=["SUSTAIN",phase,i+1,trialn,response,accuracy,n]
-            subjectdata.append(trialdata)
-            write_file(directory,subjectdata,',')
-    generalization(model,data)
-
-##########################################################
-  #SALMA: Generalization phase:
-##########################################################
-def generalization(model,data):
-    nitemscorrect = 0
-    phase='generalization'
-    random.shuffle(dataitems)
-    for j in dataitems:
-        trialn=int(floor(j[0][1]))
-        [res,prob,outunits,outacts,act,dist] = model.stimulate(j, env)
-        [lambdas, clus, conn, response, accuracy, n] = model.learnunsupervised(j,env)
-        trialdata=["SUSTAIN",phase,1,trialn,response,accuracy,n]
-        subjectdata.append(trialdata)
-        write_file(directory,subjectdata,',')
-
-##########################################################
-  #SALMA: Writes results to csv file:
-##########################################################
-def write_file(filename,data,delim):
-    datafile=open(filename,'w')
-    datafile.write("Model, Phase, Block, Item, Response, Accuracy, Clusters number" + '\n')
-    for i in data:
-        si = ', '.join(str(ie) for ie in i)
-        line=str(si)+delim+'\n'
-        datafile.write(line)
-    datafile.close()
-
-###########################################################
-# main
-###########################################################
-def main():
-    model = SUSTAIN(r = 5.0, beta = 3.97491, d = 6.514972, 
-            threshold = 0.95, learn = 0.1150532,
-            initalphas = array([1.0]*len(data[0]),float64) )
-    training(model, data)
-    open(directory,"r")
-        
-###########################################################
-# let's start
-###########################################################
-
-if __name__ == '__main__':
-    main() 
